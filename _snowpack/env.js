cdd44fbfb6ddc87139b65f752c411e6212462037
johnb8005 <johnb8005@users.noreply.github.com>
2022-03-03 21:37:37 +0000

    deploy: faf98276199aa36f5c31f13b4a104e548939c869

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "4258c7ffa0e706f93566abea888ec148ebf30067";
-export const SNOWPACK_PUBLIC_VERSION = "v0.0.6";
+export const SNOWPACK_PUBLIC_GIT_SHA = "faf98276199aa36f5c31f13b4a104e548939c869";
+export const SNOWPACK_PUBLIC_VERSION = "v0.0.7";
 export const SNOWPACK_PUBLIC_URL = "/openapi-to-idl";
 export const MODE = "production";
 export const NODE_ENV = "production";
